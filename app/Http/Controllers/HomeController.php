2dd13a8a2f6d1f72ab8e2c319544cb1f4e25bb42
Derk van der Heide <derkvanderheide@hotmail.com>
2020-12-04 16:02:06 +0100

    add category links to blog title

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,19 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Post;
+use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     /**
      * Show the index page
      */
-    public function index()
+    public function index(Request $request)
     {
-        $posts = Post::orderBy('created_at', 'desc')->paginate(4);
+        $categoryId = $request->query('categoryId');
+
+        if (!empty($category)) {
+            $category = Category::find($categoryId);
+            $posts = $category->posts;
+        } else {
+            $posts = Post::orderBy('created_at', 'desc');
+        }
 
         return view('home.index', [
-            'posts' => $posts,
+            'posts' => $posts->paginate(4),
         ]);
     }
 }
