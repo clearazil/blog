18b58c175d142b1902fd5385a53110927f7ecc76
Derk van der Heide <derkvanderheide@hotmail.com>
2020-12-11 14:24:30 +0100

    retrieve posts via ajax

--- public/js/app.js
+++ public/js/app.js
@@ -6,45 +6,62 @@ const postsWithoutCategoryLink = document.querySelector(
 
 postsWithoutCategoryLink.onclick = (event) => {
     event.preventDefault();
-    // do some ajax call
+
+    const search = new URLSearchParams({
+        uncategorized: 1,
+    });
+
+    displayPosts(search);
 };
 
 let categoryId;
 const main = document.querySelector('#main');
+
+/**
+ *
+ * @param {URLSearchParams} search
+ */
+function displayPosts(search) {
+    main.innerHTML = '<img src=/images/loading.gif>';
+
+    const url = new URL(route('home.index'));
+    url.search = search;
+
+    fetch(url, {
+        headers: {
+            'Accept': 'application/json, text-plain, */*',
+            'X-Requested-With': 'XMLHttpRequest',
+        },
+        method: 'get',
+    })
+        .then((response) => {
+            if (response.ok) {
+                return Promise.resolve(response);
+            } else {
+                return Promise.reject(new Error('Failed to load'));
+            }
+        })
+        .then((response) => {
+            return response.text();
+        }).then((data) => {
+            main.innerHTML = data;
+        }).catch((error) => {
+            main.innerHTML = '<p>Er is een fout opgetreden.</p>';
+            console.log(`Error: ${error.message}`);
+        });
+}
 
 document.addEventListener('click', (event) => {
     if (event.target &&
         event.target.classList.contains('category-link')
     ) {
         event.preventDefault();
-        main.innerHTML = '<img src=/images/loading.gif>';
         categoryId = event.target.getAttribute('category-id');
+        const search = new URLSearchParams({
+            categoryId: categoryId,
+        });
 
-        console.log(categoryId);
-
-        fetch(route('home.index'), {
-            headers: {
-                'Accept': 'application/json, text-plain, */*',
-                'X-Requested-With': 'XMLHttpRequest',
-            },
-            method: 'get',
-        })
-            .then((response) => {
-                if (response.ok) {
-                    return Promise.resolve(response);
-                } else {
-                    return Promise.reject(new Error('Failed to load'));
-                }
-            })
-            .then((response) => {
-                console.log(response);
-                return response.text();
-            }).then((data) => {
-                main.innerHTML = data;
-                console.log(data);
-            }).catch((error) => {
-                console.log(`Error: ${error.message}`);
-            });
+        displayPosts(search);
     }
 });
 
